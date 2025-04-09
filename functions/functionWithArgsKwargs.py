3706b702fd91bbdd35b42ad5d20353399ac9bccc
ekaterina <vakula.yo@gmail.com>
2025-04-09 21:55:40 +0300

    noref any function task

--- functions/functionWithArgsKwargs.py
+++ functions/functionWithArgsKwargs.py
@@ -1,23 +1,10 @@
 def add(a, b):
-   # print(str(a) + ' ' + str(b) + ' ddddddddddddd')
     return a + b
 
 
 def call(func, *args, **kwargs):
-    if len(args) == 2: return func(args[0], args[1])
-    elif len(args) == 1:
-        return max(*args)
-    else:
-        #print(kwargs['a'])
-        #print(kwargs['b'])
-        return kwargs['a'] + kwargs['b']
-
-
+    return func(*args, **kwargs)
 
 print(call(add, 2, 3))
-print(call(add, a = 4, b = 1))
-print(call(max, [3,2,4,7]))
-
-
-
-
+print(call(add, a=4, b=1))
+print(call(max, [3, 2, 4, 7]))
