2ddfd77d3d5eadf5c049cd46a0a312cedae48208
ekaterina <vakula.yo@gmail.com>
2025-04-11 21:02:22 +0300

    noref pets name by owner

--- TuplesAndDictionaries/PetsName.py
+++ TuplesAndDictionaries/PetsName.py
@@ -1,16 +1,16 @@
-def dog_owners(pets_list):
+def dog_owners(pets):
     res = dict()
-    for i in pets_list:
-        res[(i[1], i[2], i[3])] = res.get((i[1], i[2], i[3]), []) + list((i[0],))
+    for pet, name, surname, age in pets:
+        res[(name, surname, age)] = res.get((name, surname, age), []) + [pet]
     return res
 
-pets = [("Fido", "John", "Malkovic", 22),
+petss = [("Fido", "John", "Malkovic", 22),
         ("Butch", "Jake", "Smirnoff", 18),
         ("Zooma", "Simon", "Ng", 32),
         ("Chase", "Martha", "Black", 73),
         ("Rocky", "Simon", "Ng", 32)]
 
-print(dog_owners(pets))
+print(dog_owners(petss))
 
 #should be:
 # == {("John", "Malkovic", 22): ["Fido"],
