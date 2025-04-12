8c55bbecfc76baefdc8f33a661adc6dbb9f8f5c1
ekaterina <vakula.yo@gmail.com>
2025-04-12 18:23:10 +0300

    noref pets name change age

--- TuplesAndDictionaries/PetsName.py
+++ TuplesAndDictionaries/PetsName.py
@@ -17,4 +17,24 @@ print(dog_owners(petss))
 #                     ("Jake", "Smirnoff", 18): ["Butch"],
 #                     ("Simon", "Ng", 32): ["Zooma", "Rocky"],
 #                     ("Martha", "Black", 73): ["Chase"]
-#                     })
+#                     }
+
+
+def update_age(owners, owner, new_age):
+    if owners.get(owner) and new_age != owner[2]:
+        owners[(owner[0], owner[1], new_age)] = owners.pop(owner)
+
+dog_owners = {("John", "Malkovic", 22): ["Fido"],
+              ("Jake", "Smirnoff", 18): ["Butch"],
+              ("Simon", "Ng", 32): ["Zooma", "Rocky"],
+              ("Martha", "Black", 73): ["Chase"]
+              }
+
+
+update_age(dog_owners, ("Jake", "Smirnoff", 18), 22)
+update_age(dog_owners, ("Jake", "Smirnoff", 22), 23)
+update_age(dog_owners, ("Jake", "Smirnoff", 30), 24)
+print(dog_owners.get(("Jake", "Smirnoff", 22)))
+print(dog_owners.get(("Jake", "Smirnoff", 18)))
+print(dog_owners.get(("Jake", "Smirnoff", 23)))
+print(dog_owners.get(("Jake", "Smirnoff", 24)))
