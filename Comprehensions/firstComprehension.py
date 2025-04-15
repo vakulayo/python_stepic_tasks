2ed5333d81128e3ae9adb4bf482d4c6b27c85eab
ekaterina <vakula.yo@gmail.com>
2025-04-15 19:38:21 +0300

    noref more complex list comprehension tasks

--- Comprehensions/firstComprehension.py
+++ Comprehensions/firstComprehension.py
@@ -1,16 +1,27 @@
 # list from 1 to 2000
 a = [i + 1 for i in range(2000)]
-print(a)
+# print(a)
 
 # list from 1 to 10000 div 3
 b = [i for i in range(1, 10001) if i % 3 == 0]
-print(b)
+# print(b)
 
 # list from input
-c = [int(i) for i in input().split(' ')]
+ssss = '3 4 5 2 4'
+c = [int(i) for i in ssss.split(' ')]
 print(c)
 
-
 # list of squares of input and show position
-f = {k: int(v)**2 for (k,v) in enumerate(input().split())}
+sssss = '34 23 56 7 2'
+f = {k: int(v)**2 for (k,v) in enumerate(sssss.split())}
 print(f)
+
+
+# list of string with string after :
+s = '70:ztuebs 0:zdhkpq 54:jkyfmo 16:ducsvpq 85:ntsaw'
+print([v for (k, v) in enumerate(s.replace(':', ' ').split(' ')) if k % 2 != 0])
+print([x.split(':')[1] for x in s.split()])
+
+# dict with key - numbers, values - strings
+ss = '73:yjanlou 82:fsbohtg 58:xfuln 1:zshywk'
+print({int(word.split(':')[0]): word.split(':')[1] for word in ss.split()})
