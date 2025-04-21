760bb480e53d5c192fec40d83c56ce8b3406bb92
ekaterina <vakula.yo@gmail.com>
2025-04-21 20:00:19 +0300

    noref file read and write

---
+++ Files/ReadFile.py
@@ -0,0 +1,35 @@
+#number of lines in a file
+f = open('/home/ekaterina/Downloads/file.txt', 'r')
+counter = 0
+for string in f:
+    counter+=1
+f.close()
+print(counter)
+
+#number of chars in a file
+f2 = open('/home/ekaterina/Downloads/file.txt', 'r')
+counter2 = 0
+for st in f2:
+    counter2+=len(st)
+f2.close()
+print(counter2)
+
+
+#sum of numbers from file
+def sum_numbers(filename):
+    counter1 = 0
+    with open(filename, 'r') as f1:
+        for st1 in f1:
+            counter1+=int(st1)
+    return counter1
+
+print(sum_numbers('/home/ekaterina/Downloads/file.txt'))
+
+
+#add message to file
+def write_message(filename, message):
+    with open(filename, 'w') as f3:
+        f3.write(message)
+    pass
+
+write_message('/home/ekaterina/Downloads/file.txt', 'I hate Trump')
