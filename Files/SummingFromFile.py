9c1e9b25933ef52e26d190ce21b8a51b0de01f80
ekaterina <vakula.yo@gmail.com>
2025-05-02 14:58:15 +0300

    noref coordinates from file

--- Files/SummingFromFile.py
+++ Files/SummingFromFile.py
@@ -7,3 +7,16 @@ def total_sum(filename):
 
 
 print(total_sum('/home/ekaterina/Downloads/file1.txt'))
+
+
+def minmax_coords(filename):
+    all_x, all_y = [], []
+    with open(filename) as file:
+        for line in file:
+            if line.splitlines() != ['']:
+                all_x.append(int(line.split()[0]))
+                all_y.append(int(line.split()[1]))
+    return min(all_x, default=None), max(all_x, default=None), min(all_y, default=None), max(all_y, default=None)
+
+
+print(minmax_coords('/home/ekaterina/Downloads/points1.txt'))
