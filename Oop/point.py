6540189a673300d12e907ba0c9baa1f1d44de322
ekaterina <vakula.yo@gmail.com>
2025-05-06 13:42:51 +0300

    noref rich comparison

--- Oop/point.py
+++ Oop/point.py
@@ -13,7 +13,6 @@ class Point:
         return ((self.x - point.x) ** 2 + (self.y - point.y) ** 2) ** 0.5
 
 
-
 p1 = Point(1.5, 1)
 p2 = Point(1.5, 2)
 print(p1.dist(p2))
@@ -25,7 +24,7 @@ class Circle:
         self.radius = radius
 
     def square(self):
-        return 3.14*self.radius**2
+        return 3.14 * self.radius ** 2
 
     def get_center(self):
         return self.center
@@ -33,15 +32,37 @@ class Circle:
     def get_raduis(self):
         return self.radius
 
-    def  do_intersect(self, other_circle):
-        if self.center.dist(other_circle.center) > (self.radius + other_circle.radius): return False
-        else: return True
+    def do_intersect(self, other):
+        return self.center.dist(other.center) <= (self.radius + other.radius)
+
+    def __eq__(self, other):
+        return self.radius == other.radius
+
+    def __lt__(self, other):
+        return self.radius < other.radius
+
+    def __le__(self, other):
+        return self.radius <= other.radius
+
+    def __ne__(self, other):
+        return self.radius != other.radius
+
+    def __ge__(self, other):
+        return self.radius >= other.radius
+
+    def __gt__(self, other):
+        return self.radius > other.radius
 
 
 print(Circle(Point(0, 0), 0).square())
 print(Circle(Point(0, 0), 1).square())
 print(Circle(Point(120, -324), 43).square())
-print( 3.14 * 43 * 43)
+print(3.14 * 43 * 43)
 
-print(Circle(Point(0,0), 1).do_intersect(Circle(Point(3,4), 1)))
-print(Circle(Point(0,0), 1).do_intersect(Circle(Point(3,4), 5)))
+print(Circle(Point(0, 0), 1).do_intersect(Circle(Point(3, 4), 1)))
+print(Circle(Point(0, 0), 1).do_intersect(Circle(Point(3, 4), 5)))
+circle1 = Circle(Point(0, 0), 1)
+circle2 = Circle(Point(3, 4), 5)
+print(circle1.__eq__(circle2))
+print(circle1.__eq__(circle1))
+print(circle1.__gt__(circle2))
