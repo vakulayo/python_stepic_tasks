ab7af9b5806090e35882869734fefe425965c87f
ekaterina <vakula.yo@gmail.com>
2025-05-06 14:50:04 +0300

    noref add other classes for game

--- Oop/game.py
+++ Oop/game.py
@@ -32,8 +32,43 @@ class Wall(GameObject):
         self.passable = False
         self.interactable = False
 
-coin = Coin(0 ,0 , 'ddd')
+
+coin = Coin(0, 0, 'ddd')
 print(coin.render())
 
-wall = Wall(0,0, 'wall')
+wall = Wall(0, 0, 'wall')
 print(wall)
+
+
+class Player(GameObject):
+    def render(self):
+        return '@'
+
+
+class SoftWall(GameObject):
+    def render(self):
+        return '%'
+
+    def __init__(self, x, y, game):
+        super().__init__(x, y, game)
+        self.passable = False
+        self.interactable = True
+
+class HeatWave(GameObject):
+    def render(self):
+        return '+'
+
+    def process(self):
+        self.alive = False
+        pass
+
+    def interact(self, object):
+        if type(object) == Player or type(object) == SoftWall: object.alive = False
+        pass
+
+
+player = Player(3, 4, 'ddd')
+heatwave = HeatWave(5, 6, 'ddd')
+
+heatwave.interact(player)
+print(player)
