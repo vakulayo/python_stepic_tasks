e3ace342d58ff28f7ca2b8647bdafe91f7bd172e
ekaterina <vakula.yo@gmail.com>
2025-05-06 14:31:14 +0300

    noref classes for game

---
+++ Oop/game.py
@@ -0,0 +1,39 @@
+class GameObject:
+    def __init__(self, x, y, game):
+        self.y = y
+        self.x = x
+        self.game = game
+        self.alive = True
+        self.passable = True
+        self.interactable = True
+
+    def interact(self, object):
+        pass
+
+    def process(self):
+        pass
+
+    def render(self):
+        raise NotImplementedError
+
+
+class Coin(GameObject):
+    def render(self):
+        return "$"
+
+
+class Wall(GameObject):
+    def render(self):
+        return '#'
+
+    def __init__(self, x, y, game):
+        super().__init__(x, y, game)
+        self.alive = True
+        self.passable = False
+        self.interactable = False
+
+coin = Coin(0 ,0 , 'ddd')
+print(coin.render())
+
+wall = Wall(0,0, 'wall')
+print(wall)
